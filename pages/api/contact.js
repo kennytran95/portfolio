0b0324a415f4b4500119e12baf7275682d969695
Kenny <Kennyytran95@gmail.com>
2022-07-23 00:11:06 -0700

    add confirmation messsage

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -37,5 +37,5 @@ export default async function sendMessage(req, res) {
   } catch (err) {
     console.log(err);
   }
-  res.status(200).json(req.body);
+  res.status(200).send("Successfully emailed Kenny Tran! Woot woot!");
 }
