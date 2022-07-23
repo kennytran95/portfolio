83d71fb26ce5808af1fb80913e40f48935288a0a
Kenny <Kennyytran95@gmail.com>
2022-07-22 17:32:25 -0700

    title

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { NavBar } from "../components/NavBar.jsx";
 import { Banner } from "../components/Banner.jsx";
 import { Skills } from "../components/Skills.jsx";
@@ -9,6 +10,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Kenny Tran | Full Stack Engineer</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <NavBar />
       <Banner />
       <Skills />
